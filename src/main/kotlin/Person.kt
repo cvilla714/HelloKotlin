ac145f17c6bdd6a89d9673763e66bac2b5046b29
Cosmel Villalobos <cosmelvg@gmail.com>
2022-02-19 20:42:45 -0500

    Adding a class
    working with classes and how to pass the constructor

---
+++ src/main/kotlin/Person.kt
@@ -0,0 +1,9 @@
+//class Person(_firstName:String, _lastName:String) {
+//    val firstName :String = _firstName
+//    val lastName: String = _lastName
+//
+//}
+
+//another way to achieve the same thing
+class Person(val firstName:String, val lastName:String) {
+}
--- src/main/kotlin/arrays.kt
+++ src/main/kotlin/arrays.kt
@@ -2,27 +2,34 @@ fun main() {
     val nbaTeams = arrayOf("Lakers","Warriors","Clippers","Bucks")
 //    printing the amount of elements inside the array
     println(nbaTeams.size)
+println()
 //    printing the first element
-    println()
     println(nbaTeams[0])
+println()
 //    printing the first element again
-    println()
     println(nbaTeams.get(0))
+println()
 //    using a for loop
-    println()
     for (item in nbaTeams){
         println(item)
     }
+
 //    using a foreach for iterating the array
-    println()
+println()
     nbaTeams.forEach {
         println(it)
     }
-
+println()
 //    renaming the it inside the foreach
-    println()
     nbaTeams.forEach {item ->
         println(item)
     }
+println()
+//    using the class person
+    val person = Person("Luffy","Monkey D")
+    println(person.lastName)
+    println(person.firstName)
 
 }
+
+
